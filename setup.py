8add091891ad7c7e3847ce8a2b915cb7dc7ba94c
Rohan-D19 <70074790+Rohan-D19@users.noreply.github.com>
2021-10-24 10:21:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
+import csv
+import numpy as np
 
+
+def getDataSource(data_path):
+  marks_in_percentages = []
+  days_present = []
+  with open(data_path) as csv file:
+    csv_reader = csv.DictReader(csv_file)
+  for row in csv_reader:
+    marks_in_percentages = append(float(row["Marks in Percentage"]))
+    days_present = append(float(row["Days Present']))
+         
+  return ("x":marks_in_percentages, "y" : days_present)
+                                    
+                                    
+def findCorrelation(dataSource):
+    correlation = np.corrcoef(datasource["x"], datasource["y"])        
+    print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
+
+def setup:
+    data_path = "./data/Student Marks vs Days Present.csv"
+                               
+dataSource = getDataSource(data_path)
+findCorrelation(dataSource)
+                                    
+setup()
